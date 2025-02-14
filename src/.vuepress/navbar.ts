1e4e10533ba6d17d304b03aa3f41df75d3fec60a
capybara x <511598384@qq.com>
2025-02-14 16:12:07 +0800

    feat(main): capybara-docs: 项目初始化。

---
+++ src/.vuepress/navbar.ts
@@ -0,0 +1,17 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/",
+  "/front-end/",
+  "/blog/",
+  {
+    text: "其他",
+    icon: "other",
+    children: [
+      {
+        text: "软考",
+        children: ["/other/rk/gx/"],
+      }
+    ]
+  } 
+]);
